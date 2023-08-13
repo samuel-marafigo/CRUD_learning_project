5bbb7ad4be36beaf729bde89bb630e8e8e256d50
samuel-marafigo <125743714+samuel-marafigo@users.noreply.github.com>
2023-08-13 15:09:46 -0300

    "Include" and "list" functions added in main.py according to course schedule. Created placeholder message for functions that are still being implemented. Removed redundant menu options.

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import time
-def menu_principal():
+
+estudantes = []  # cria lista para registro de estudantes
+
+
+def menu_principal():  # layout e lógica do menu principal
     print('=== MENU PRINCIPAL ===')
     print('(1) Gerenciar estudantes.')
     print('(2) Gerenciar professores.')
@@ -7,174 +11,20 @@ def menu_principal():
     print('(4) Gerenciar turmas.')
     print('(5) Gerenciar matrículas.')
     print('(9) Sair.')
-
-def estudantes():
-    while True:
-        print('(1) Incluir.')
-        print('(2) Listar.')
-        print('(3) Atualizar.')
-        print('(4) Excluir.')
-        print('(9) Voltar ao menu principal.')
-        try:
-            opcao = int(input('Escolha uma opção: '))
-            if opcao == 1:
-                incluir()
-            elif opcao == 2:
-                listar()
-            elif opcao == 3:
-                atualizar()
-            elif opcao == 4:
-                excluir()
-            elif opcao == 9:
-                break
-            else:
-                print('Opção inválida!')
-        except ValueError:
-            print('Opção inválida! Você precisar escolher um *número*.')
-            time.sleep(1)
-
-
-def professores():
-    while True:
-        print('(1) Incluir.')
-        print('(2) Listar.')
-        print('(3) Atualizar.')
-        print('(4) Excluir.')
-        print('(9) Voltar ao menu principal.')
-        try:
-            opcao = int(input('Escolha uma opção: '))
-            if opcao == 1:
-                incluir()
-            elif opcao == 2:
-                listar()
-            elif opcao == 3:
-                atualizar()
-            elif opcao == 4:
-                excluir()
-            elif opcao == 9:
-                break
-            else:
-                print('Opção inválida!')
-        except ValueError:
-            print('Opção inválida! Você precisar escolher um *número*.')
-            time.sleep(1)
-
-def matriculas():
-    while True:
-        print('(1) Incluir.')
-        print('(2) Listar.')
-        print('(3) Atualizar.')
-        print('(4) Excluir.')
-        print('(9) Voltar ao menu principal.')
-        try:
-            opcao = int(input('Escolha uma opção: '))
-            if opcao == 1:
-                incluir()
-            elif opcao == 2:
-                listar()
-            elif opcao == 3:
-                atualizar()
-            elif opcao == 4:
-                excluir()
-            elif opcao == 9:
-                break
-            else:
-                print('Opção inválida!')
-        except ValueError:
-            print('Opção inválida! Você precisar escolher um *número*.')
-            time.sleep(1)
-
-
-def disciplinas():
-    while True:
-        print('(1) Incluir.')
-        print('(2) Listar.')
-        print('(3) Atualizar.')
-        print('(4) Excluir.')
-        print('(9) Voltar ao menu principal.')
-        try:
-            opcao = int(input('Escolha uma opção: '))
-            if opcao == 1:
-                incluir()
-            elif opcao == 2:
-                listar()
-            elif opcao == 3:
-                atualizar()
-            elif opcao == 4:
-                excluir()
-            elif opcao == 9:
-                break
-            else:
-                print('Opção inválida!')
-        except ValueError:
-            print('Opção inválida! Você precisar escolher um *número*.')
-            time.sleep(1)
-
-def turmas():
-    while True:
-        print('(1) Incluir.')
-        print('(2) Listar.')
-        print('(3) Atualizar.')
-        print('(4) Excluir.')
-        print('(9) Voltar ao menu principal.')
-        try:
-            opcao = int(input('Escolha uma opção: '))
-            if opcao == 1:
-                incluir()
-            elif opcao == 2:
-                listar()
-            elif opcao == 3:
-                atualizar()
-            elif opcao == 4:
-                excluir()
-            elif opcao == 9:
-                break
-            else:
-                print('Opção inválida!')
-        except ValueError:
-            print('Opção inválida! Você precisar escolher um *número*.')
-            time.sleep(1)
-
-def incluir():
-    print('Você escolheu: Incluir')
-    exit()
-
-def listar():
-    print('Você escolheu: Listar')
-    exit()
-
-def atualizar():
-    print('Você escolheu: Atualizar')
-    exit()
-
-def excluir():
-    print('Você escolheu: Excluir')
-    exit()
-
-while True:
-    menu_principal()
     try:
         opcao = int(input('Escolha uma opção: '))
         if opcao == 1:
             print('Você escolheu gerenciar: Estudantes')
             time.sleep(1)
-            estudantes()
+            menu_estudantes()
         elif opcao == 2:
-            print('Você escolheu gerenciar: Professores')
-            time.sleep(1)
-            professores()
+            desenvolvimento()
         elif opcao == 3:
-            print('Você escolheu gerenciar: Disciplinas')
-            time.sleep(1)
-            disciplinas()
+            desenvolvimento()
         elif opcao == 4:
-            print('Você escolheu gerenciar: Turmas')
-            time.sleep(1)
-            turmas()
+            desenvolvimento()
         elif opcao == 5:
-            print('Você escolheu gerenciar: Matrículas')
-            time.sleep(1)
-            matriculas()
+            desenvolvimento()
         elif opcao == 9:
             exit()
         else:
@@ -182,17 +32,61 @@ while True:
             time.sleep(1)
             print('Retornando ao menu principal...')
             time.sleep(1)
-    except ValueError:
+    except ValueError:  # adicionado um block de try-except para não quebrar o código caso o usuário não digite um int.
         print('Opção inválida! Você precisar escolher um *número*.')
         time.sleep(1)
 
 
+def menu_estudantes():  # layout e lógica do menu de operações - estudantes
+    while True:
+        print('(1) Incluir.')
+        print('(2) Listar.')
+        print('(3) Atualizar.')
+        print('(4) Excluir.')
+        print('(9) Voltar ao menu principal.')
+        try:
+            opcao = int(input('Escolha uma opção: '))
+            if opcao == 1:
+                incluir()
+            elif opcao == 2:
+                listar()
+            elif opcao == 3:
+                desenvolvimento()
+            elif opcao == 4:
+                desenvolvimento()
+            elif opcao == 9:
+                break
+            else:
+                print('Opção inválida!')
+        except ValueError:
+            print('Opção inválida! Você precisar escolher um *número*.')
+            time.sleep(1)
 
 
+def desenvolvimento():  # placeholder das funções ainda não implementadas
+    print('Em desenvolvimento')
+    time.sleep(1)
+    print('Retornando ao menu anterior...')
+    time.sleep(1)
 
 
+def incluir():  # operação de inclusão
+    print('Você escolheu: Incluir')
+    nome_estudante = input('Informe o nome do estudante: ')
+    estudantes.append(nome_estudante)
 
 
+def listar():  # operação de listagem
+    print('Você escolheu: Listar')
+    if len(estudantes) > 0:
+        print('=== LISTA ===')
+        for i in range(len(estudantes)):
+            print(estudantes[i])
+        time.sleep(2)
+    else:
+        print('Lista vazia...')
+        time.sleep(1)
 
 
-
+while True:  # loop principal
+    menu_principal()
